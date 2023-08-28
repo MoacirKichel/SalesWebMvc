d387b36338073535f89be445712b2c4af586e31c
MoacirKichel <moakichel@gmail.com>
2023-08-28 15:09:02 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -7,60 +7,69 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} required")]
         public string Email { get; set; }
+
+
         [Display(Name = "Birth Date")]
+        [Required(ErrorMessage = "{0} required")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
-    public double BaseSalary { get; set; }
+        [Required(ErrorMessage = "{0} required")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
+        public double BaseSalary { get; set; }
 
-    public Department Department { get; set; }
+        public Department Department { get; set; }
 
-    public int DepartmentId { get; set; }
+        public int DepartmentId { get; set; }
 
-    public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
-    public Seller()
-    {
+        public Seller()
+        {
+        }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public Seller(string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSalles(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+
+        public void RemoveSalles(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        public double TotalSalles(DateTime inicial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= inicial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
     }
-
-    public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
-    {
-        Id = id;
-        Name = name;
-        Email = email;
-        BirthDate = birthDate;
-        BaseSalary = baseSalary;
-        Department = department;
-    }
-
-    public Seller(string name, string email, DateTime birthDate, double baseSalary, Department department)
-    {
-        Name = name;
-        Email = email;
-        BirthDate = birthDate;
-        BaseSalary = baseSalary;
-        Department = department;
-    }
-
-    public void AddSalles(SalesRecord sr)
-    {
-        Sales.Add(sr);
-    }
-
-    public void RemoveSalles(SalesRecord sr)
-    {
-        Sales.Remove(sr);
-    }
-
-    public double TotalSalles(DateTime inicial, DateTime final)
-    {
-        return Sales.Where(sr => sr.Date >= inicial && sr.Date <= final).Sum(sr => sr.Amount);
-    }
-}
 }
 
